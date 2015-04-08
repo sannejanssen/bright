3f75f23dac664bb981fd5487c655402d6ba545f4
Sanne Janssen <info@sannejanssen.be>
2015-04-08 21:39:21 +0200

    add 'under construction' page

---
+++ www/index.php
@@ -0,0 +1,76 @@
+
+<!doctype html>
+<html lang="nl" dir="ltr">
+  <head>
+    <title>Welcome | Bright Venture Lab</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="title" content="Welcome" />
+    <meta name="description" content="Bright is a no nonsense, strategic advisory firm devoted to making other businesses flourish. Instigating sustainable growth and entrepreneurship, we aim to be the best long-term strategic partner for our clients and partners, as well as the environment and society we live in." />
+    <meta name="MobileOptimized" content="width">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
+    <meta http-equiv="cleartype" content="on">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <style>
+
+    body {
+      font-size: 16px;
+    }
+
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+
+    figure {
+      margin: 0 auto;
+      max-width: 16em;
+      margin-top: 20%;
+    }
+
+    h1 {
+      display: none;
+      visibility: hidden;
+      margin: 0;
+    }
+
+    p {
+      margin: 1em 0 0 0;
+      font-size: 1em;
+
+      text-align: center;
+      color: #d2d3d5;
+    }
+
+    @media screen and (min-width: 768px) {
+      figure {
+        max-width: 26em;
+        margin-top: 20%;
+      }
+    }
+
+    @media screen and (min-width: 1024px) {
+      figure {
+        max-width: 600px;
+        margin-top: 15%;
+      }
+
+      p {
+        margin-top: 3em;
+      }
+    }
+
+    </style>
+  </head>
+  <body>
+    <div class="page">
+      <figure class="logo">
+        <img width="600" src="logo-bright-venture-lab.jpg" alt="Logo Bright Venture Lab" />
+      </figure>
+      <h1>Bright Venture Lab</h1>
+      <p>Coming soon</p>
+    </div>
+
+  </body>
+</html>
